3c420672b07715b9e9a2e31db6d4b32ad2a6e485
Helen Haveloh <h.havelohh@gmail.com>
2024-10-04 15:08:04 +0200

    added show-shoppinglist-function

--- shoppinglist.py
+++ shoppinglist.py
@@ -6,3 +6,13 @@ def add_item():
     item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
     shoppinglist.append(item)
     print("Der Artikel wurde zum Einkaufswagen hinzugefügt.")
+
+# definiere eine Funktion show_shoppinglist
+def show_shoppinglist():
+    # Überprüfe, ob shoppinglist nicht leer ist
+    if (shoppinglist):
+        print("Deine Einkaufsliste: ")
+        for item in shoppinglist:
+            print(f"{item}")
+    else:
+        print("Die Einkaufsliste ist leer")
